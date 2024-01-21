4908861de88c634dbedb576fb279e5148ec60c35
LorenaGRB <71951729+LorenaGRB@users.noreply.github.com>
2024-01-21 15:50:02 +0000

    feat(styles): initial colors have been added

---
+++ constants/styles.ts
@@ -0,0 +1,17 @@
+export const Colors = {
+    white: '#FFFFFF',
+    black: '#000000',
+    ['gray-200']:'#F1F1F1',
+    ['gray-300']:'#FEFEFE',
+    ['gray-400']:'#CDC7C7',
+    ['gray-700']:'#333333',
+    cream:'#CDC7C7',
+    ['pink-200']: '#F4CAE0', 
+    ['pink-300']: '#F7D0D3',
+    ['pink-400']: '#F6CBB0',
+    ['pink-500']: '#E4CBCB',
+    ['pink-700']: '#F18B8B',
+    ['pink-900']: '#C40909',
+    ['purple-500']: '#B4AACD',
+    ['purple-800']: '#75728D',
+}
