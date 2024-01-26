eb0ed4ed19403b02e1760cb223501c5881bfad5c
LorenaGRB <lorenagabby4@gmail.com>
2024-01-26 20:42:39 +0100

    prueba2

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import GameDashboard from './screens/GameDashboard'
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import GameDashboard from "./screens/GameDashboard";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>prueba</Text>
       <StatusBar style="auto" />
       <GameDashboard />
     </View>
@@ -15,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
